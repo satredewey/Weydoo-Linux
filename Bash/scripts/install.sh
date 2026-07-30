71e719b9f1d99f86e1a9a638768bb3646fd5b073
Satre Dewey <satre.dewey@lyceemaillolperpignan.fr>
2026-07-30 15:26:55 +0200

    Remove Python installation

--- Bash/scripts/install.sh
+++ Bash/scripts/install.sh
@@ -1,8 +1,1 @@
-# echo "Updating APT cache..."
-# apt update  > /dev/null
-# echo "Upgrading APT packages..."
-# apt upgrade -y > /dev/null
-# echo "Installing Python..."
-# apt install python3 -y > /dev/null
-
 python3 Bash/scripts/install.py
