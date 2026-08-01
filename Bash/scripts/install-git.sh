cc675f21e0ebdd1c725ece98000e75923fed9f27
Satre Dewey <satre.dewey@lyceemaillolperpignan.fr>
2026-08-01 16:58:39 +0200

    Removed all .sh install scripts. Make a bin.

--- Bash/scripts/install-git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if command -v git >/dev/null 2>&1; then
-    echo "Git is already installed" >&2
-    exit 0
-fi
-
-sudo add-apt-repository ppa:git-core/ppa
-sudo apt update; sudo apt install git
--- Bash/scripts/install-icons-in-terminal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p tmp; pushd tmp
-git clone https://github.com/sebastiencs/icons-in-terminal.git --depth=1
-pushd icons-in-terminal
-sh install.sh
-source $HOME/.bashrc
-popd; popd
-rm -rf tmp
