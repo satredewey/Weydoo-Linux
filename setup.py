0d6289118617e46ce215f3b09280458a8b53db39
Satre Dewey <satre.dewey@lyceemaillolperpignan.fr>
2026-08-02 11:38:23 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ import sys
 import subprocess
 
 if not os.path.exists(".venv"):
-    subprocess.run([sys.executable, "-m", "venv", ".venv"], check=True)
+    try:
+        subprocess.run([sys.executable, "-m", "venv", ".venv"], check=True)
+    except subprocess.CalledProcessError:
+        print("The venv extension is missing, installing via apt...")
+        subprocess.run(["sudo", "apt", "update"], check=True)
+        subprocess.run(["sudo", "apt", "install", "-y", "python3-venv"], check=True)
+        subprocess.run([sys.executable, "-m", "venv", ".venv"], check=True)
 
 venv_python = os.path.join(".venv", "bin", "python")
 venv_pip = os.path.join(".venv", "bin", "pip")
@@ -14,6 +20,5 @@ if sys.executable != os.path.abspath(venv_python):
     os.execv(venv_python, [venv_python] + sys.argv)
 
 import pyfiglet
-
 print(pyfiglet.figlet_format("Succes !", font="ansi_shadow"))
-print("\nRun 'source .venv/bin/activate' before runnings scripts.")
+print("Run 'source .venv/bin/activate' before running scripts.")
